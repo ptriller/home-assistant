1a5e408db75dd76bfdb046d05b72b22098371afa
Peter Triller <peter.triller@1und1.de>
2019-05-27 17:38:40 +0200

    working reconnect

--- script/lgtest.py
+++ script/lgtest.py
@@ -12,7 +12,6 @@ logger.addHandler(logging.StreamHandler())
 
 printer = pprint.PrettyPrinter(indent=2)
 
-result_handler = {}
 
 
 class LGTVComponent:
@@ -21,74 +20,83 @@ class LGTVComponent:
         self._terminate = False
         self._uri = uri
         self._websocket = None
+        self._msg_handler = {}
 
     async def reconnect(self):
         try:
             self._websocket = await websockets.connect(self._uri)
-        except TimeoutError:
+        except:
             return False
         return True
 
     async def ensure_connection(self, callback=None):
         if self._websocket is not None:
             return
-        for i in range(0, 1):
+        for i in range(0, 2):
             await asyncio.sleep(i)
+            print("Try Online Reconnect")
             if self._terminate:
                 return
             if await self.reconnect():
                 if callback is not None:
-                    asyncio.create_task(callback())
+                    asyncio.get_event_loop().create_task(callback())
                 return
 #        self.state_offline()
         delay = 0
         while True:
+            print("Try Offline Reconnect with delay " + str(delay))
             if delay < 10:
                 delay += 1
             await asyncio.sleep(delay)
             if self._terminate:
                 return
             if await self.reconnect():
-                if callback is not None:
-                    asyncio.create_task(callback())
+                asyncio.get_event_loop().create_task(self.message_loop())
                 return
 
     async def message_loop(self):
         try:
-#            self.register_listeners()
-            async for message in self._websocket:
+            while True:
+                message = await self._websocket.recv()
                 printer.pprint(message)
-        except Exception:
+        except:
             if self._terminate:
                 return
             self._websocket = None
-            asyncio.create_task(self.ensure_connection(self.message_loop))
+            asyncio.get_event_loop().create_task(self.ensure_connection())
 
     async def ping(self):
         while True:
             if self._terminate:
                 return
-            if self._websocket is not None:
-                poll = await self._websocket.ping()
-                await poll
+            try:
+                if self._websocket is not None:
+                    async def pingpong():
+                        pong = await self._websocket.ping()
+                        await pong
+                    await asyncio.wait_for(pingpong(), timeout=3)
+            except:
+                print("Ping timeout, reconnecting")
+                asyncio.get_event_loop().create_task(self._websocket.close())
             await asyncio.sleep(1)
 
     async def connect(self):
-        asyncio.create_task(self.ensure_connection(self.message_loop))
-        asyncio.create_task(self.ping())
+        asyncio.get_event_loop().create_task(self.ensure_connection())
+        asyncio.get_event_loop().create_task(self.ping())
 
     def terminate(self):
         self._terminate = True
         if self._websocket is not None:
-            asyncio.create_task(self._websocket.close())
+            ws = self._websocket
             self._websocket = None
+            asyncio.get_event_loop().create_task(ws.close())
 
     def start(self):
         asyncio.get_event_loop().run_until_complete(
             self.connect())
 
 
-tv = LGTVComponent("ws://192.168.2.113:3000")
+tv = LGTVComponent("ws://localhost:8080/ws")
 
 tv.start()
 
@@ -98,5 +106,5 @@ async def stop():
     tv.terminate()
 
 
-#asyncio.get_event_loop().run_until_complete(stop())
+asyncio.get_event_loop().run_until_complete(stop())
 asyncio.get_event_loop().run_forever()
